7be69935de097b61c12df73e244c67b9d84726d5
Evan Kuhn <ejkuhn@gmail.com>
2026-08-19 16:46:10 -0700

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.py
@@ -0,0 +1,39 @@
+class Solution:
+    def calc_volume(self, heights: List[int], a: int, b: int) -> int:
+        '''
+        Calculate volume given heights and two indexes.
+        '''
+        return (b - a) * min(heights[a], heights[b])
+
+    def maxArea(self, heights: List[int]) -> int:
+        # Initialize indexes a,b
+        n = len(heights)
+        #print(f"n={n}")
+        a = 0
+        b = n-1
+
+        # Save the pointer values (a,b) that yield the highest volume thus far
+        best_a = a
+        best_b = b
+        best_vol = self.calc_volume(heights, best_a, best_b)
+        #print(f"Init best: h[{a}]={heights[a]}, h[{b}]={heights[b]}, vol={best_vol}")
+
+        while(True):
+            # Move the shorter pointer
+            if heights[a] < heights[b]:
+                a += 1
+            else:
+                b -= 1
+            
+            # Quit if pointers cross
+            if a >= b:
+                break
+
+            # Calculate new volume. Store it if it is better.
+            temp_vol = self.calc_volume(heights, a, b)
+            if temp_vol > best_vol:
+                best_a = a
+                best_b = b
+                best_vol = temp_vol
+
+        return best_vol
