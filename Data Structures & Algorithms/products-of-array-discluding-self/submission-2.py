c016ed6d4a4f96d1c006c5cd8c0da0724cfaa400
Evan Kuhn <ejkuhn@gmail.com>
2026-08-21 23:17:19 -0700

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,58 @@
+class Solution:
+    # Using division will fail if there are zeroes, since zero will wipe 
+    # out any info on what numbers were multiplied together.
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        #print(f"nums: {nums}")
+        
+        # PASS 1:
+        # Keep an array where the value at index i is the product
+        # of all elements to the left, i.e. in range [0,i).
+        products_left = nums.copy()
+        for i in range(0, n):
+            if i == 0:
+                products_left[i] = 1
+            elif i == 1:
+                products_left[i] = nums[i-1]
+            else:
+                products_left[i] = nums[i-1] * products_left[i-1]
+
+        #print(f"products_left: {products_left}")
+
+        # PASS 2:
+        # Build same array for rightward products
+        products_right = nums.copy()
+        for i in range(n-1, -1, -1):
+            if i == n-1:
+                products_right[i] = 1
+            elif i == n-2:
+                products_right[i] = nums[i+1]
+            else:
+                products_right[i] = nums[i+1] * products_right[i+1]
+                
+        #print(f"products_right: {products_right}")
+
+        # PASS 3:
+        # Compute results: value at index i = products to left + products to right
+        results = [0] * n
+        for i in range(0, n):
+            if i == 0:
+                results[i] = products_right[i]
+            elif i == n-1:
+                results[i] = products_left[i]
+            else:
+                results[i] = products_left[i] * products_right[i]
+
+        return results
+
+
+
+            
+
+            
+
+
+        
+        
