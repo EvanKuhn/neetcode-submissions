a04ec53e35f75e868e0d594095289de9486984e7
Evan Kuhn <ejkuhn@gmail.com>
2026-08-26 16:17:37 -0700

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.py
@@ -0,0 +1,36 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+        
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            # Check if target out of bounds
+            # if target < nums[l]:
+            #     return -1
+            # if target > nums[r]:
+            #     return -1
+
+            # Check if found at edges
+            if target == nums[l]:
+                return l
+            if target == nums[r]:
+                return r
+
+            # Compute middle index
+            m = l + ((r - l) // 2)
+
+            # Go right or left
+            # - Ensure that l and r change by at least 1
+            if target < nums[m]:
+                r = min(m, r-1)
+            elif target > nums[m]:
+                l = max(m, l+1)
+            else:
+                return m
+
+        return -1
